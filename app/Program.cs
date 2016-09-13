150bbe12946b879c58cd2e8b4343a1bd19e0c4ec
Edgard <eavilesmejia@gmail.com>
2016-09-12 21:37:14 -0600

    using volumen to work with src code

---
+++ app/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
+
